dd507f4c20649d412ba29d7a8875c462e4ce80fd
Jordan <jordmsimm@gmail.com>
2018-05-19 14:22:39 -0400

    add scss styling

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -26,7 +26,7 @@ export default class AddOption extends React.Component{
                 {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option"></input>
-                    <button >Submit</button>
+                    <button className="button" >Add Option</button>
                 </form>
             </div>
         ) ;
